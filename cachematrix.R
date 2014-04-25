3e21a60e231b1daf4a82de93c09146c473e4fde7
Paulo Freitas <pauloroberto_lds@yahoo.com>
2014-04-24 21:49:13 -0300

    included my r code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These function calculate the inverse of a matriz, but if it was previously
+##calculated, it returns the result of the first calculation
 
-## Write a short comment describing this function
+#To execute these functions type create a matriz enviroment, for example:
+#m <- makeCacheMatrix(matrix(c(1, 2, 3, 4), nrow=2,ncol=2))
+#Then run the functions codes below
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,setsolve = setsolve,getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+#finally call the function, typing:
+#cacheSolve(m)
+##the function will return a matrix that is the inverse of 'x'
+
+
+#if you type again 
+#cacheSolve(m)
+##the function will return the value previouly calculated.
